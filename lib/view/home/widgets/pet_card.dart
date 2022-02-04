dbaf1f194a4febccbe40c2d24ae5445577c95706
Felipe Ribeiro <feliper.dev@gmail.com>
2022-02-04 18:11:23 -0300

    Route

--- lib/view/home/widgets/pet_card.dart
+++ lib/view/home/widgets/pet_card.dart
@@ -12,91 +12,96 @@ class PetCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height * .17,
-      width: size.width * .7,
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(14),
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          LayoutBuilder(
-            builder: (context, constraints) {
-              return Container(
-                height: constraints.maxHeight,
-                width: 120,
-                margin: const EdgeInsets.only(right: 16),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(12),
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/');
+      },
+      child: Container(
+        height: size.height * .17,
+        width: size.width * .7,
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            LayoutBuilder(
+              builder: (context, constraints) {
+                return Container(
+                  height: constraints.maxHeight,
+                  width: 120,
+                  margin: const EdgeInsets.only(right: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                );
+              },
+            ),
+            Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  pet.name!,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 20,
+                  ),
                 ),
-              );
-            },
-          ),
-          Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                pet.name!,
-                style: const TextStyle(
-                  fontWeight: FontWeight.w900,
-                  fontSize: 20,
+                const SizedBox(height: 10),
+                Text(
+                  pet.type!,
+                  style: const TextStyle(),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                pet.type!,
-                style: const TextStyle(),
-              ),
-              const SizedBox(height: 6),
-              Row(
-                children: [
-                  Text(
-                    controller.getPetSex(pet.sex!),
-                    style: const TextStyle(
-                      color: AppColors.textGrey,
+                const SizedBox(height: 6),
+                Row(
+                  children: [
+                    Text(
+                      controller.getPetSex(pet.sex!),
+                      style: const TextStyle(
+                        color: AppColors.textGrey,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 6),
-                  Text(
-                    '${pet.age}, ${controller.getPetAge(pet.petAge!)}',
-                    style: const TextStyle(
-                      color: AppColors.textGrey,
+                    const SizedBox(width: 6),
+                    Text(
+                      '${pet.age}, ${controller.getPetAge(pet.petAge!)}',
+                      style: const TextStyle(
+                        color: AppColors.textGrey,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 12),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Icon(
-                    Icons.location_on,
-                    color: AppColors.favoriteIcon,
-                    size: 20,
-                  ),
-                  const SizedBox(width: 3),
-                  Text(
-                    '${pet.distance} ${controller.getPetDistance(pet.petDistanceType!)}',
-                    style: const TextStyle(
-                      color: AppColors.textGrey,
+                  ],
+                ),
+                const SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Icon(
+                      Icons.location_on,
+                      color: AppColors.favoriteIcon,
+                      size: 20,
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const Spacer(),
-          Icon(pet.favorite! ? Icons.favorite : Icons.favorite_outline,
-              color:
-                  pet.favorite! ? AppColors.favoriteIcon : AppColors.textGrey,
-              size: 28),
-        ],
+                    const SizedBox(width: 3),
+                    Text(
+                      '${pet.distance} ${controller.getPetDistance(pet.petDistanceType!)}',
+                      style: const TextStyle(
+                        color: AppColors.textGrey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const Spacer(),
+            Icon(pet.favorite! ? Icons.favorite : Icons.favorite_outline,
+                color:
+                    pet.favorite! ? AppColors.favoriteIcon : AppColors.textGrey,
+                size: 28),
+          ],
+        ),
       ),
     );
   }
